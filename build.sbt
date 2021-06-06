edba23e6ae4490834daade2721dfcdf474cca715
Mac <mac@Dhineshs-MBP.domain.name>
2021-06-06 18:38:58 +0530

    First Commit

---
+++ build.sbt
@@ -0,0 +1,42 @@
+name := "SparkIntegration"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+val sparkVersion = "3.0.2"
+val kafkaVersion = "2.4.0"
+val log4jVersion = "2.4.1"
+val nlpLibVersion = "3.5.1"
+
+resolvers ++= Seq(
+  "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven",
+  "Typesafe Simple Repository" at "https://repo.typesafe.com/typesafe/simple/maven-releases",
+  "MavenRepository" at "https://mvnrepository.com"
+)
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+
+  // streaming
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+
+  // streaming-kafka
+  "org.apache.spark" % "spark-sql-kafka-0-10_2.12" % sparkVersion,
+
+  // low-level integrations
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion,
+
+  // logging
+  "org.apache.logging.log4j" % "log4j-api" % log4jVersion,
+  "org.apache.logging.log4j" % "log4j-core" % log4jVersion,
+
+  "edu.stanford.nlp" % "stanford-corenlp" % nlpLibVersion,
+  "edu.stanford.nlp" % "stanford-corenlp" % nlpLibVersion classifier "models",
+
+  // kafka
+  "org.apache.kafka" %% "kafka" % kafkaVersion,
+  "org.apache.kafka" % "kafka-streams" % kafkaVersion
+)
+
---
+++ src/main/scala/sparkIntegration/IntegratingKafka.scala
@@ -0,0 +1,70 @@
+package sparkIntegration
+
+import common.carsSchema
+import org.apache.spark.sql.functions.{col, expr, struct, to_json}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object IntegratingKafka {
+
+  val spark = SparkSession.builder()
+    .appName("Integrating Kafka")
+    .master("local[2]")
+    .getOrCreate()
+
+  def readFromKafka() = {
+    // https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html
+    val kafkaDF: DataFrame = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "dhinesh")
+      .load()
+
+    kafkaDF
+     .select(col("topic"), expr("cast(value as string) as actualValue"))
+      .writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
+  }
+
+  def writeToKafka() = {
+    val carsDF = spark.readStream
+      .schema(carsSchema)
+      .json("src/main/resources/data/cars")
+
+    val carsKafkaDF = carsDF.selectExpr("upper(Name) as key", "Name as value")
+
+    carsKafkaDF.writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "dhinesh")
+      .option("checkpointLocation", "checkpoints") // without checkpoints the writing to Kafka will fail
+      .start()
+      .awaitTermination()
+  }
+
+  def writeCarsToKafka() = {
+    val carsDF = spark.readStream
+      .schema(carsSchema)
+      .json("src/main/resources/data/cars")
+
+    val carsJsonKafkaDF = carsDF.select(
+      col("Name").as("key"),
+      to_json(struct(col("Name"), col("Horsepower"), col("Origin"))).cast("String").as("value")
+    )
+
+    carsJsonKafkaDF.writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "dhinesh")
+      .option("checkpointLocation", "checkpoints")
+      .start()
+      .awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    writeCarsToKafka()
+  }
+
+}
